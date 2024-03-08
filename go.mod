2347bab2e0ef144e6024123c0f1168d26f7ae308
Mike Fridman <mfridman@buf.build>
2024-03-08 11:23:57 -0500

    initial commit

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/mfridman/go-scratch
+
+go 1.22.1
+
+require github.com/bufbuild/protoplugin v0.0.0-20240307214627-634912f18796
+
+require google.golang.org/protobuf v1.32.0 // indirect
---
+++ pkg/foo/main.go
@@ -0,0 +1,17 @@
+package foo
+
+import (
+	"context"
+
+	"github.com/bufbuild/protoplugin"
+)
+
+// Handle implements the [https://pkg.go.dev/github.com/bufbuild/protoplugin#Handler] interface and
+// is the entry point for the plugin.
+func Handle(
+	ctx context.Context,
+	responseWriter *protoplugin.ResponseWriter,
+	request *protoplugin.Request,
+) error {
+	return nil
+}
